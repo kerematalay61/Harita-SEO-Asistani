e78cee67165b3f93e31635ff8ba7354e43b5a2c7
kerematalay61 <keremazra61@gmail.com>
2025-05-10 04:32:57 +0300

    Create log_writer.py

---
+++ log_writer.py
@@ -0,0 +1,6 @@
+import datetime
+
+def write_log(message):
+    with open("logs/activity_log.txt", "a", encoding="utf-8") as log_file:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_file.write(f"[{timestamp}] {message}\n")
