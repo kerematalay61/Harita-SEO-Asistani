c754b68e827664dac116cf025354361cfd67bef8
kerematalay61 <keremazra61@gmail.com>
2025-05-10 05:40:17 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+DEFAULT_LANGUAGE = "tr"
+DEFAULT_THEME = "light"
+SUPPORT_EMAIL = "destek@haritaseoasistani.com"
+APP_NAME = "Harita SEO Asistanı"
+
+EMAIL_SUBJECTS = {
+    "verify": "Doğrulama Kodu",
+    "welcome": "Kayıt Başarılı",
+    "reset": "Şifre Sıfırlama"
+}
+
+PIN_LIMIT_PER_USER = 2000
+COMMENT_LIMIT_PER_USER = 1000
