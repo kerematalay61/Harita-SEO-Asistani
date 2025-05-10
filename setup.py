72def1fc06d4230ab948a0aff13368be1c5af216
kerematalay61 <keremazra61@gmail.com>
2025-05-10 05:57:16 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HaritaSEOAsistani",
+    version="1.0.0",
+    author="Kerem Atalay",
+    author_email="keremazra61@gmail.com",
+    description="Google Haritalar için AI destekli yorum ve pinleme aracı.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Flask",
+        "requests",
+        "openai",
+        "faker",
+        "python-dotenv",
+        "beautifulsoup4",
+        "selenium",
+        "webdriver-manager",
+        "pillow",
+        "smtplib",
+        "email-validator",
+        "schedule",
+        "user-agents",
+        "bcrypt"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+git add setup.py
+git commit -m "setup.py dosyası eklendi"
+git push origin main
